fd865dd494f31a60514315984993811d2ac11706
onitaiji4real <minecraft8810@gmail.com>
2024-05-04 20:16:12 +0800

    feat: add Issue interface & list data.title

--- app/another/page.tsx
+++ app/another/page.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Navbar from "../(home)/components/Navbar";
 import MaxWidthWrapper from "@/components/Layout/MaxWidthWrapper";
 import TheHeader from "@/components/Layout/TheHeader";
+import useSWR from "swr";
+import axios from "axios";
+import { issueFetcher } from "@/lib/utils/fetcher";
 
-export default function page() {
+export default function Page() {
+  interface Issue {
+    id: number;
+    title: string;
+    body: string;
+  }
+
+  const url = "https://api.github.com/repos/onitaiji4real/blog-pages-db/issues";
+
+  const { data, error, isLoading } = useSWR<Issue[]>(url, issueFetcher);
+
   return (
     <>
       <TheHeader />
       <div className="h-10 xl:h-32  bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full"></div>
+      <div>
+        {!isLoading && data ? (
+          data.map((issue) => <h2 key={issue.id}>{issue.title}</h2>)
+        ) : (
+          <p>isLoading...</p>
+        )}
+      </div>
     </>
   );
 }
